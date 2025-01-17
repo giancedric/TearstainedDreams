494b3e2cac06f243e37dbc62a8e7f9e00d05df56
Gian <gncdrc@gmail.com>
2025-01-17 14:28:38 -0700

    tearstaineddreams

---
+++ new-game-project/scripts/global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var player_current_attack = false
+
+var current_scene = "world" #world town
+var transition_scene = false
+
+var player_exit_house_posx = 0
+var player_exit_house_posy = 0
+
+var player_start_house_posx = 0
+var player_start_house_posy = 0
