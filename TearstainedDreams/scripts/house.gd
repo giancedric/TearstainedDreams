1a6a680a8d9505bb32f44332e4a7170a9ad25625
giancedric <gbele931@evit.edu>
2025-01-22 19:59:50 -0700

    added dialogue with entity and fixed movement while in dialogue

--- TearstainedDreams/scripts/house.gd
+++ TearstainedDreams/scripts/house.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var dialogue_shown = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,7 +19,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	change_scenes()
 
-
 func _on_house_transition_point_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		global.transition_scene = true
@@ -36,6 +36,21 @@ func _on_house_cutscene_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		$entity.visible = true
 		$entity.play("summon")
+		global.game_pause = true
+
+func _on_house_cutscene_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		$entity.play("disappear")
+		await $entity.animation_finished
+		$entity.visible = false
+		if $entity.visible == false:
+			$tear.play("default")
+			$tear.visible = true
+			
 
 func _on_entity_animation_finished() -> void:
+	global.game_pause = false
 	$entity.play("idle")
+	if $entity.animation == "idle" and not dialogue_shown:
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/main.dialogue"), "houseentity")
+		dialogue_shown = true
---
+++ TearstainedDreams/scripts/player.gd
@@ -0,0 +1,84 @@
+extends CharacterBody2D
+
+
+const SPEED = 65.0
+
+var current_dir = "none"
+
+
+func player():
+	pass
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+	current_camera()
+	
+func player_movement(_delta):
+	if global.game_pause == false:
+		if Input.is_action_pressed("right"):
+			current_dir = "right" 
+			play_anim(1)
+			velocity.x = SPEED
+			velocity.y = 0
+		elif Input.is_action_pressed("left"):
+			current_dir = "left"
+			play_anim(1) 
+			velocity.x = -SPEED
+			velocity.y = 0
+		elif Input.is_action_pressed("down"):
+			current_dir = "down"
+			play_anim(1)  
+			velocity.y = SPEED
+			velocity.x = 0
+		elif Input.is_action_pressed("up"):
+			current_dir = "up"
+			play_anim(1) 
+			velocity.y = -SPEED
+			velocity.x = 0
+		else:
+			play_anim(0) 
+			velocity.x = 0
+			velocity.y = 0
+	elif global.game_pause == true:
+		velocity.x = 0
+		velocity.y = 0
+		play_anim(0)
+	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	
+	if dir == "right":
+		if movement == 1:
+			anim.play("right_walk")
+		elif movement == 0:
+			anim.play("right_idle")
+	if dir == "left":
+		if movement == 1:
+			anim.play("left_walk")
+		elif movement == 0:
+			anim.play("left_idle")
+	if dir == "down":
+		
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
+
+func current_camera():
+	if global.current_scene == "house":
+		$housecam.enabled = true
+		$worldcam.enabled = false
+	elif global.current_scene == "world":
+		$housecam.enabled = false
+		$worldcam.enabled = true
