5f1dc0bead53345fea3e2028acbde6fbb59e18be
Gian <gncdrc@gmail.com>
2025-01-23 14:32:09 -0700

    owo

--- TearstainedDreams/scripts/house.gd
+++ TearstainedDreams/scripts/house.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var dialogue_shown = false
+var player_in_area = false
+var tear_collected = false
+var cutscene_played = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,6 +21,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	change_scenes()
+	if player_in_area and $tear.visible:
+		$interacttext.visible = true
+		collect_tear()
+	else:
+		$interacttext.visible = false
 
 func _on_house_transition_point_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
@@ -33,13 +41,15 @@ func change_scenes():
 
 
 func _on_house_cutscene_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
+	if body.has_method("player") and not cutscene_played:
 		$entity.visible = true
 		$entity.play("summon")
 		global.game_pause = true
+		
 
 func _on_house_cutscene_body_exited(body: Node2D) -> void:
-	if body.has_method("player"):
+	if body.has_method("player") and not cutscene_played:
+		cutscene_played = true
 		$entity.play("disappear")
 		await $entity.animation_finished
 		$entity.visible = false
@@ -54,3 +64,18 @@ func _on_entity_animation_finished() -> void:
 	if $entity.animation == "idle" and not dialogue_shown:
 		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/main.dialogue"), "houseentity")
 		dialogue_shown = true
+
+
+func _on_collectarea_body_entered(body: Node2D) -> void:
+	if body.has_method("player") and $tear.visible == true:
+		player_in_area = true
+
+
+func _on_collectarea_body_exited(body: Node2D) -> void:
+	if body.has_method("player") and $tear.visible == true:
+			player_in_area = false
+
+func collect_tear():
+	if Input.is_action_just_pressed("interact"):
+		tear_collected = true
+		$tear.visible = false
