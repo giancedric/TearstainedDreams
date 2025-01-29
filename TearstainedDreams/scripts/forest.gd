f5c30ea22466b18fff672ee940c0b11bddf9a9da
giancedric <gbele931@evit.edu>
2025-01-28 21:07:45 -0700

    fixed ysorting in forest

--- TearstainedDreams/scripts/forest.gd
+++ TearstainedDreams/scripts/forest.gd
@@ -7,4 +7,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	path()
+
+func path():
+	if global.housetear_collected==true:
+		if Input.is_action_pressed("path"):
+			$path.visible = true
+		elif Input.is_action_just_released("path"):
+			$path.visible = false
