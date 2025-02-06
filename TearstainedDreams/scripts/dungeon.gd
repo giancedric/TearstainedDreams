499e6468d2de3ae8b0303286fb383171959254e0
Gian <gncdrc@gmail.com>
2025-02-06 14:33:12 -0700

    finished decorating dungeon

--- TearstainedDreams/scripts/dungeon.gd
+++ TearstainedDreams/scripts/dungeon.gd
@@ -3,16 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	global.current_scene = "dungeon"
+	global.game_pause = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-
-func _on_dooropens_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
-		$door.visible = false
-		$dooropen.visible = true
