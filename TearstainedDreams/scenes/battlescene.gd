3b1a6c28d05bf2a951629ddf756ad14b1a545093
Gian <gncdrc@gmail.com>
2025-02-21 13:41:16 -0700

    Added buttons to the panel

--- TearstainedDreams/scenes/battlescene.gd
+++ TearstainedDreams/scenes/battlescene.gd
@@ -4,7 +4,7 @@ var anim_finished = false
 var spawned = false
 
 var fade_in_progress = false
-var fade_speed = 1.0  # Adjust speed (higher = faster fade)
+var fade_speed = 0.5  # Adjust speed (higher = faster fade)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -31,3 +31,15 @@ func _on_spawn_animation_finished() -> void:
 		$VBoxContainer/enemy.visible = true
 		fade_in_progress = true
 		$VBoxContainer/enemy.play()
+
+
+func _on_attack_pressed() -> void:
+	print("attack pressed")
+
+
+func _on_defend_pressed() -> void:
+	print("defend pressed")
+
+
+func _on_run_pressed() -> void:
+	print("run pressed")
